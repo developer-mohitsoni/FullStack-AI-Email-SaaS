cefff8c3446ceaed7fe72463dba718ae653af42c
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-21 18:29:18 +0530

    Add /api/clerk/webhook route to demonstrate how the data is coming from clerk to the webhook that ping to our server when new user is created

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,11 @@
+// /api/clerk/webhook
+
+export const POST = async (req: Request) => {
+  const { data } = await req.json();
+
+  console.log("clerk webhook received", data);
+
+  return new Response("Webhook received", {
+    status: 200,
+  });
+};
